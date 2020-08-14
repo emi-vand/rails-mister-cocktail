d6718a2f63a34d72a0ff086b2c2388e812584750
Emilia Vandamme <emilia.audrey@gmail.com>
2020-08-14 14:22:48 +0100

    adding extras

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -21,12 +21,17 @@ class CocktailsController < ApplicationController
     @dose = Dose.new
   end
 
+  def destroy
+    @cocktail = Cocktail.find(params[:id])
+    @cocktail.destroy
+    redirect_to cocktails_path
+  end
 
   private
 
 
   def cocktail_params
-    params.require(:cocktail).permit(:name)
+    params.require(:cocktail).permit(:name )
   end
 
 end
