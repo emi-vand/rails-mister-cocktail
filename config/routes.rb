a7b20a48b6c0276bda71ae3b2b5216235555ece2
Emilia Vandamme <emilia.audrey@gmail.com>
2020-08-18 10:16:23 +0100

    few more design changes

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,5 @@ Rails.application.routes.draw do
   resources :cocktails, only: [:index, :show, :new, :create, :destroy] do
     resources :doses, only: [:new, :create]
   end
-  root to: 'cocktails#index'
+  root to: 'pages#home'
 end
